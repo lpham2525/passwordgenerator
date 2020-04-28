6af22f12af851c429ab7b2e4cb86685bffee2002
LanChi Pham <lpham2525@gmail.com>
2020-04-27 23:08:33 -0400

    made revisions to javascript

--- script.js
+++ script.js
@@ -10,46 +10,47 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", () => );
+generateBtn.addEventListener("click", () => {
+  const length = parseInt(prompt("Choose a length for your password between 8-128 characters."));
+  while (length < 8 || length > 128) {
+    alert("Please choose a number between 8 and 128.")
+    return
+  }
 
-const genLength = parseInt(prompt("Choose a length for your password between 8-128 characters."));
-if (genLength < 8 || genLength > 128) {
-  alert("Please choose a number between 8 and 128.")
-  return
-}
+  const strOne = '0123456789'
+  const strTwo = 'abcdefghijklmnopqrstuvwxyz'
+  const strThree = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const strFour = "!#$%&'()*+,-./:;<=>?@[]\\^_`{|}~"
+  let newPassword = ' '
+  const numbersElem = confirm("Would you like numbers in your password?")
+  const lowercaseElem = confirm("Would you like lowercase letters in your password?")
+  const uppercaseElem = confirm("Would you like uppercase letters in your password?")
+  const specialCharactersElem = confirm("Would you like special characters in your password?")
+  let passwordPlace = ' '
 
-const strOne = '0 1 2 3 4 5 6 7 8 9'
-const strTwo = 'abcdefghijklmnopqrstuvwxyz'
-const strThree = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const strFour = "!#$%&'()*+,-./:;<=>?@[]\\^_`{|}~"
-let newPassword = ' '
-const numbersElem = confirm("Would you like numbers in your password?")
-const lowercaseElem = confirm("Would you like lowercase letters in your password?")
-const uppercaseElem = confirm("Would you like uppercase letters in your password?")
-const specialCharactersElem = confirm("Would you like special characters in your password?")
-let passwordPlace = ' '
+  if (lowercaseElem === false && uppercaseElem === false && numbersElem === false && specialCharactersElem === false) {
+    alert("Choose at least one criteria to generate password.")
+    return
+  }
 
-if (!lowercaseElem && !uppercaseElem && !numbersElem && !specialCharactersElem) {
-  alert("Choose at least one criteria to generate password.")
-  return
-}
+  if (numbersElem) {
+    passwordPlace += strOne;
+  }
+  if (lowercaseElem) {
+    passwordPlace += strTwo;
+  }
+  if (uppercaseElem) {
+    passwordPlace += strThree;
+  }
+  if (specialCharactersElem) {
+    passwordPlace += strFour;
+  }
+  console.log('passwordPlace: ', passwordPlace)
+  for (let i = 0; i < length; i++) {
+    let chosen = Math.floor(Math.random() * passwordPlace.length)
+    newPassword += passwordPlace[chosen]
+  }
+  console.log('newPassword: ', newPassword)
+  document.getElementById('password').value = newPassword
+})
 
-if (numbersElem) {
-  passwordPlace += strOne;
-}
-if (lowercaseElem) {
-  passwordPlace += strTwo;
-}
-if (uppercaseElem) {
-  passwordPlace += strThree;
-}
-if (specialCharactersElem) {
-  passwordPlace += strFour;
-}
-
-for (let i = 0; i < genLength.length; i++) {
-  let chosen = Math.floor(Math.random() * passwordPlace.length)
-  newPassword += passwordPlace[chosen]
-}
-return newPassword
-}
